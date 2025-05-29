687c2f38448e8973394197c2809f64b6407932f3
0xilis <QuickUpdateShortcutSupport@protonmail.com>
2025-05-29 09:59:47 -0400

    fix: get rid of siri functionality for now as it's caused nothing but issues

--- Lina/AppDelegate.swift
+++ Lina/AppDelegate.swift
@@ -20,8 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = rootVC
         window?.makeKeyAndVisible()
         UIOnboardingHelper.showOnboardingIfNeeded(in: rootVC)
-        AppIntent.allowSiri()
-        AppIntent.archive()
+        //AppIntent.allowSiri()
+        //AppIntent.archive()
         return true
     }
     
